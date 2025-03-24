123d08e1789b8e57e5e8db45c0f1750a33917f69
Ismobro <ismaeelmoskinzada10@gmail.com>
2025-03-24 09:15:03 -0500

    Did some stuff

---
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace AvaloniaApplication1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        //var x = new StackPanel() { Background = Brushes.Red };
+
+    }
+}
