ce66ef0e7e261404f704301d377f30faecb01e9c
Ismobro <ismaeelmoskinzada10@gmail.com>
2025-03-25 11:35:11 -0500

    Updated stuff?

--- AvaloniaApplication1/MainWindow.axaml.cs
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -1,14 +1,60 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
-using Avalonia.Media;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media; 
 
 namespace AvaloniaApplication1;
 
 public partial class MainWindow : Window
 {
+    public static TextBlock x;  
+    //define it outside of classes to use it in different classes
     public MainWindow()
     {
         InitializeComponent();
-        //var x = new StackPanel() { Background = Brushes.Red };
+        var list = new List<string> { "test1", "test2", "test3" };
+        x = this.FindControl<TextBlock>("Tasks");
+        x.Text = string.Join('\n', list) ;
+    }
+    public void Add_Click(object sender, RoutedEventArgs args)
+    {
+        
+        var inputBoxTasks = new TextBox();
+        {
+            HorizontalAlignment = HorizontalAlignment.Center;
+            VerticalAlignment = VerticalAlignment.Bottom;
+            Background = Brushes.Transparent;
+            inputBoxTasks.AcceptsReturn = false;
+            inputBoxTasks.AcceptsTab = true;
+        }
+        
+       
+        
+        
+        if (sender is Button btn)
+        {
+            ((StackPanel)(btn.Parent)).Children.Add(inputBoxTasks);
+        }
+        
+        inputBoxTasks.KeyDown += (sender, e) =>
+        {
+            if (e.Key == Key.Enter)
+            {
+                string taskText = inputBoxTasks.Text; // Save input text to a variable
+                x.Text += "\n" + taskText;
+                // Remove TextBox from parent
+                if (inputBoxTasks.Parent is Panel parentPanel)
+                {
+                    parentPanel.Children.Remove(inputBoxTasks);
+                }
 
+                e.Handled = true; // Optional: mark event as handled
+            }
+        };
     }
 }
+//FIND A WAY TO MAKE  A SOUND WHEN YOU ADD OR REMOVE A TASK
+
+// Handle Enter key press
